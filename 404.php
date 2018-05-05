ca9934f3f7b3d3d100192545876c5764b6492710
Anatolii <anatolii.lukavenko@gmail.com>
2018-05-05 13:09:51 +0300

    Made layout 404 page

---
+++ 404.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?> 
+<!--END HEADER -->
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12 col-xs-12">
+			<div class="col-sm-8 col-sm-offset-2 col-xs-12">
+				<div class="content-not-found-container">
+					<h1>Error</h1>
+					<i class="fas fa-frown"></i>
+					<h2>Page not found</h2>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Footer -->
+
+<?php get_footer(); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
